2da6b40f055e6ccf25399574ccb2cfe37b0c070b
Lilo Onwuzu <lpr422@gmail.com>
2016-04-06 18:52:53 -0700

    Handles multiple input fields but only dispays more info on the last one

--- js/scripts.js
+++ js/scripts.js
@@ -25,85 +25,76 @@ function Address(street, state, country){
 
   Address.prototype.fullAddress = function(){
     // .fullAddress() is a prototype of address construct to compute address for each instantiation of address for each input field batch
+    // To use the .fullAddress() prototype, you have to first define an object as an instantiation of the Address construct(object) using var object = new Construct
     return this.street + ", " + this.state + ", " + this.country;
   };
 
-function Push(year, street, state, country, notes){
-  // push construct will be used to make an array of all the information for each input field batch
+function Full(year, street, state, country, notes, countryYear, address){
+  // full construct will be used to make an array of all the information for each input field batch
   this.year = year;
   this.street = street;
   this.state = state;
   this.country = country;
   this.notes = notes;
+  this.countryYear = countryYear;
+  this.address = address;
 };
 
-  Push.prototype.pushIn = function(){
-    var blanks = [];
-    return blanks.push(this.year, this.street, this.state, this.country, this.notes);
-    // the .pushIn() prototype will return an array of the group of info from one input field batch. To use the .pushIn() prototype, you have to first define an object as an instantiation of the Push construct(object) using var object = new Construct
-  };
-
 function reset(){ //reset function empties the input fields. This is an object function that can be called later on in the UI logic
-  $("input.year").val("");
-  $("input.street").val("");
-  $("input.state").val("");
-  $("input.country").val("");
-  $("input.notes").val("");
+  $("input.new-year").val("");
+  $("input.new-street").val("");
+  $("input.new-state").val("");
+  $("input.new-country").val("");
+  $("input.new-notes").val("");
 };
 
 // User Interface Logic (collects input info and displays output)
 $(document).ready(function(){ // when the document is refreshed and finishes loading...
 
-  $(".add").click(function(event){
+  $(".addField").click(function(event){
     // when the document loads, user should be able to add as many input fields as wanted to the form
-    $("form#places").append('<div class="inputBatch">' +
+    $("#inputBatch").append('<div class="new-inputBatch">' +
                   '<div class="form-group">' +
                     '<label for="year">What year did you visit this place:</label>' +
-                    '<input class="year" type="text">' +
+                    '<input id="year" type="text" class= "new-year">' +
                   '</div>' +
                   '<div class="form-group">' +
                     '<label for="street">What is the address of the place you stayed:</label>' +
-                    '<input class="street" type="text">' +
+                    '<input id="street" type="text" class= "new-street">' +
                   '</div>' +
                   '<div class="form-group">' +
                     '<label for="state">Enter the state which you have visited here:</label>' +
-                    '<input class="state" type="text">' +
+                    '<input id="state" type="text" class= "new-state">' +
                   '</div>' +
                   '<div class="form-group">' +
                     '<label for="country">Enter a country you have visited here:</label>'+
-                    '<input class="country" type="text">' +
+                    '<input id="country" type="text" class= "new-country">' +
                   '</div>' +
                   '<div class="form-group">' +
                     '<label for="notes">Please enter some memorable notes about this visit:</label>' +
-                    '<input class="notes" type="text">' +
+                    '<input id="notes" type="text"  class= "new-notes">' +
                   '</div>' +
                 '</div>');
   });
 
-  $(".remove").click(function(){
+  $(".removeField").click(function(){
     // when the document loads, user should be able to hide the appended batch of input fields starting with the last one
-    $("form#places").children(".inputBatch").last().remove();
+    $("#inputBatch").children(".new-inputBatch").last().remove();
+    // hide class new-inputbatch to remove the appended lists only but not id inputbatch so as to prevent hiding the original input field batch
   });
 
   $("form#places").submit(function(event){
     // at any point when the document loads, user should be able to submit the form
     event.preventDefault(); // callback function to hold submitting the form every cycle
 
-    reset(); // function call to empty input fields after you hit submit
+    $(".new-inputBatch").each(function(){
+      // this is an each loop. For every new-inputBatch class which constitutes the initial field batch and all the appended ones, compute the following
 
-    $(".inputBatch").each(function(){
-      // this is an each loop. Find inputBatch class. For each inputBatch class, compute the following
-
-      var importYear = parseInt($(this).find("input#year").val()); // .val() imports as a string unless parseInt'd. At any point when you hit submit, transfer in the data from the input fields
-      console.log(importYear);
+      var importYear = parseInt($(this).find("input#year").val());
       var importStreet = $(this).find("input#street").val();
       var importState = $(this).find("input#state").val();
-      console.log(importState);
       var importCountry = $(this).find("input#country").val();
       var importNotes = $(this).find("input#notes").val();
-      var importAll = new Push(importYear, importStreet, importState, importCountry, importNotes); // declare importAll as an instantiation of the Push object/construct
-      var outputarray = importAll.pushIn(); // use the .pushIn() prototype to create an array of all the info from one input field batch
-
       var newCountryYear = new CountryYear(importCountry, importYear);
       // newCountryYear is an instantiation of the Countryyear construct and computes a new object newCountryYear to collect the country and year info from this group
       var outputCountryYear = newCountryYear.fullPlace();
@@ -111,22 +102,30 @@ $(document).ready(function(){ // when the document is refreshed and finishes loa
       var newAddress = new Address(importStreet, importState, importCountry);
       // newAddress is an instantiation of the Address construct and computes a new object newAddress to collect the street, state and country info of this group
       var outputAddress = newAddress.fullAddress();
-      //  The .fullAddress() prototype is applied to "this" instantiation of Address to concatenate
+      // The .fullAddress() prototype is applied to "this" instantiation of Address to concatenate
+      var newFull = new Full(importYear, importStreet, importState, importCountry, importNotes, outputCountryYear, outputAddress); // declare newFull as an instantiation of the Full object/construct that gathers all the info for one batch
+      // var infoArray = blanks.push(newFull); // use the .pushIn() prototype to create an array of all the info from one input field batch
+      var outputArray = [];
+      outputArray.push(importYear, importStreet, importState, importCountry, importNotes, outputCountryYear, outputAddress);
+      console.log(outputArray);
 
-      $("#list").append(outputCountryYear);
-        // append list of places visited to placesList. Should read: Paris, 1992 for example
+      $("#list").append('<li><span class="clickToView">'+ outputCountryYear + '</span></li>');
+        // append the list of places visited to placesList. Should read: Paris, 1992 for example
 
-      $("#list").click(function(){
-        // after the countryYear items have been appended, show the items in the list to view more information
-        $(".placesInfo").show();
         $(".addPlace").text(outputCountryYear);
         $(".addAddress").text(outputAddress);
         $(".addNotes").text(importNotes);
-      });
 
+        $(".clickToView").click(function(){
+          // after the countryYear items have been appended, show the items in the list to view more information
+
+              $(".placesInfo").toggle();
+              });
     }); // end of each loop
 
     $(".placesList").show(); // show the places list when you hit submit. Everything above this executes but is still hidden until showing
+
+  reset(); // function call to empty input fields after you hit submit
 
   }); // End of submit event
 }); // End of document ready event

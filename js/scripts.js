d2617ffdded68b9132549cc80ff268d623b76ede
Lilo Onwuzu <lpr422@gmail.com>
2016-04-07 11:59:27 -0700

    Now collects all input fields but still only shows info for the most recently added item

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@
 //Prototypes: Prototypes are used to perform certain functions on any entity without having to declare each time.
 // Prototypes and object functions are followed by ()
 
+function MakeArrays(){
+  this.compile = [];
+}
+
 function CountryYear(country, year){
   // Construct. List the properties here. "this" represents every instantiation of the countryyear construct. If you define a function or method here, it can only be used on countryyear construct but not any instantiations of it. Prototypes are used to perform functions on many instantiations of a construct
   this.country = country;
@@ -83,12 +87,15 @@ $(document).ready(function(){ // when the document is refreshed and finishes loa
     // hide class new-inputbatch to remove the appended lists only but not id inputbatch so as to prevent hiding the original input field batch
   });
 
+  var newMakeArrays = new MakeArrays;
+  // this has to be a global variable so we can push series of mini-arrays to store and use to output later
+
   $("form#places").submit(function(event){
     // at any point when the document loads, user should be able to submit the form
     event.preventDefault(); // callback function to hold submitting the form every cycle
 
     $(".new-inputBatch").each(function(){
-      // this is an each loop. For every new-inputBatch class which constitutes the initial field batch and all the appended ones, compute the following
+    // this is an each loop. For every new-inputBatch class which constitutes the initial field batch and all the appended ones, compute the following. We will use this loop to compile a super array with an arbitrary number of sub-arrays that hold all the info from each input field batch.
 
       var importYear = parseInt($(this).find("input#year").val());
       var importStreet = $(this).find("input#street").val();
@@ -104,24 +111,29 @@ $(document).ready(function(){ // when the document is refreshed and finishes loa
       var outputAddress = newAddress.fullAddress();
       // The .fullAddress() prototype is applied to "this" instantiation of Address to concatenate
       var newFull = new Full(importYear, importStreet, importState, importCountry, importNotes, outputCountryYear, outputAddress); // declare newFull as an instantiation of the Full object/construct that gathers all the info for one batch
-      // var infoArray = blanks.push(newFull); // use the .pushIn() prototype to create an array of all the info from one input field batch
-      var outputArray = [];
-      outputArray.push(importYear, importStreet, importState, importCountry, importNotes, outputCountryYear, outputAddress);
-      console.log(outputArray);
 
-      $("#list").append('<li><span class="clickToView">'+ outputCountryYear + '</span></li>');
-        // append the list of places visited to placesList. Should read: Paris, 1992 for example
+      newMakeArrays.compile.push(newFull);
+      // compile is a property of MakeArrays. newMakeArrays.compile.push() wil push elements into the empty array. Create array and push in one line. Arrays have a mind if their own. newMakeArrays is the super array. newFull is the sub-array that holds all the info from each input field batch.
+      // newMakeArrays.compile === [newFull1, newFull2, ...]
 
-        $(".addPlace").text(outputCountryYear);
-        $(".addAddress").text(outputAddress);
-        $(".addNotes").text(importNotes);
-
-        $(".clickToView").click(function(){
-          // after the countryYear items have been appended, show the items in the list to view more information
-
-              $(".placesInfo").toggle();
-              });
     }); // end of each loop
+
+    newMakeArrays.compile.forEach(function(newMakeArray){
+    // forEach loop to individually display the output for all the input field batches
+    // For newFull1 implement everything below. Repeat for newFull2 and so on
+    // newMakeArrays will be called using the newMakeArray placement/placeholder
+      $("#list").append('<li><span class="clickToView">'+ newMakeArray.countryYear + '</span></li>');
+      // Here we are asking JQuery to find the list (#list) and append <li>list of places</li> to the html code. Should read: Paris, 1992 for example
+      // $(".clickToView").click(function(){
+      // // after the countryYear items have been appended, this click listener displays more information
+      //
+      //   $(".addCountryYear").text(newMakeArray.countryYear);
+      //   $(".addAddress").text(newMakeArray.address);
+      //   $(".addNotes").text(newMakeArray.notes);
+      //   // newMakeArray represents newFull1, 2 etc. countryyear, address and notes are properties of newFull(from Full construct) which is a property of newMakeArray(MakeArray construct)
+      //   $(".placesInfo").toggle();
+      });
+    });
 
     $(".placesList").show(); // show the places list when you hit submit. Everything above this executes but is still hidden until showing
 
